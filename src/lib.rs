80694cee2b1df969d7112556e6db8e762de0bfc8
Christof Fetzer <christof.fetzer@scontain.com>
2022-08-20 13:22:16 +0200

    code cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,16 @@
-use log::{info,error};
-use serde_json::{Value};
-use serde::{Deserialize, Serialize};
 use handlebars::Handlebars;
-use shells::{sh};
-use std::fs::OpenOptions;
-use rand::{thread_rng, Rng};
+use log::{error, info};
 use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use shells::sh;
+use std::env;
 use std::fs;
+use std::fs::OpenOptions;
 use std::io;
 use std::io::Write;
 use std::path::Path;
-use std::env;
-
 
 pub fn is_running_in_container() -> bool {
     // podman create /run/.containerenv inside containers
@@ -20,18 +19,22 @@ pub fn is_running_in_container() -> bool {
 }
 
 pub fn execute_scone_cli(shell: &str, cmd: &str) -> (i32, String, String) {
-
     let repo = match env::var("SCONECTL_REPO") {
-        Ok(repo) =>  repo,
-        Err(_err) =>  format!("registry.scontain.com:5050/sconectl")
+        Ok(repo) => repo,
+        Err(_err) => format!("registry.scontain.com:5050/sconectl"),
     };
 
     let vol = match env::var("DOCKER_HOST") {
-        Ok(val) => { let vol = val.strip_prefix("unix://").unwrap_or(&val).to_string(); format!(r#"-e DOCKER_HOST="{val}" -v "{vol}":"{vol}""#) },
+        Ok(val) => {
+            let vol = val.strip_prefix("unix://").unwrap_or(&val).to_string();
+            format!(r#"-e DOCKER_HOST="{val}" -v "{vol}":"{vol}""#)
+        }
         Err(_e) => format!("-v /var/run/docker.sock:/var/run/docker.sock"),
     };
 
-    let mut w_prefix = format!(r#"docker run --entrypoint="" -e "SCONECTL_REPO={repo}" --rm {vol} -v "$HOME/.docker:/root/.docker" -v "$HOME/.cas:/root/.cas" -v "$HOME/.scone:/root/.scone" -v "$PWD:/wd" -w /wd  {repo}/sconecli:latest  {cmd}"#);
+    let mut w_prefix = format!(
+        r#"docker run --entrypoint="" -e "SCONECTL_REPO={repo}" --rm {vol} -v "$HOME/.docker:/root/.docker" -v "$HOME/.cas:/root/.cas" -v "$HOME/.scone:/root/.scone" -v "$PWD:/wd" -w /wd  {repo}/sconecli:latest  {cmd}"#
+    );
 
     // we speed up calls if we already running inside of a container!
     if is_running_in_container() {
@@ -44,11 +47,14 @@ pub fn execute_scone_cli(shell: &str, cmd: &str) -> (i32, String, String) {
     };
 
     match command.output() {
-        Ok(output) => {
-            (output.status.code().unwrap_or(if output.status.success() { 0 } else { 1 }),
-             String::from_utf8_lossy(&output.stdout[..]).into_owned(),
-             String::from_utf8_lossy(&output.stderr[..]).into_owned())
-        },
+        Ok(output) => (
+            output
+                .status
+                .code()
+                .unwrap_or(if output.status.success() { 0 } else { 1 }),
+            String::from_utf8_lossy(&output.stdout[..]).into_owned(),
+            String::from_utf8_lossy(&output.stderr[..]).into_owned(),
+        ),
 
         Err(e) => (126, String::new(), e.to_string()),
     }
@@ -63,41 +69,57 @@ macro_rules! scone {
     }};
 }
 
-
-pub fn create_session<'a, T : Serialize + for<'de> Deserialize<'de>>(name : &str, hash: &str, template: &str, state : &T, force: bool) -> Result<String, &'static str> {
+pub fn create_session<'a, T: Serialize + for<'de> Deserialize<'de>>(
+    name: &str,
+    hash: &str,
+    template: &str,
+    state: &T,
+    force: bool,
+) -> Result<String, &'static str> {
     // if we already know the hash of the session, we do not try to create
     // unless we set flag force
 
-    let tmp_session_dir= "target/session_files";
+    let tmp_session_dir = "target/session_files";
     fs::create_dir_all(tmp_session_dir).expect(&format!("Failed to create  directory '{tmp_session_dir}' for session files (Error 25235-11010-6922)"));
 
     if hash.is_empty() || force {
         info!("Hash for session {} empty. Trying to determine hash.", name);
-        // we access the state object via a json "proxy" object  
+        // we access the state object via a json "proxy" object
         // - we can access fields without needing to traits... but more importantly, this enables to create session for different fields
-        let mut j : Value = serde_json::from_str(&serde_json::to_string_pretty(&state).expect("Error serializing internal state (Error 1246-28944-24836)")).expect("Error parsing session state (Error 2213-735-18099)");
+        let mut j: Value = serde_json::from_str(
+            &serde_json::to_string_pretty(&state)
+                .expect("Error serializing internal state (Error 1246-28944-24836)"),
+        )
+        .expect("Error parsing session state (Error 2213-735-18099)");
 
         let tmp_name = format!("{tmp_session_dir}/{}", random_name(20));
-        let (code,stdout, stderr) = scone!("scone session read {} > {}", name, tmp_name);
+        let (code, stdout, stderr) = scone!("scone session read {} > {}", name, tmp_name);
         let mut do_create = force; // create session, if force is set
         let mut r = Err("Incorrect code");
         if code == 0 {
             info!("Got session {} .. verifying session now ", name);
-            let (code,stdout, stderr) = scone!("scone session verify {}", tmp_name);
+            let (code, stdout, stderr) = scone!("scone session verify {}", tmp_name);
             let _ = fs::remove_file(tmp_name);
             if code == 0 {
-                info!("OK: verified  session {}: predecessor='{}'", name, stdout.clone());
+                info!(
+                    "OK: verified  session {}: predecessor='{}'",
+                    name,
+                    stdout.clone()
+                );
                 j["predecessor_key"] = "predecessor".into();
                 j["predecessor"] = stdout.clone().into();
             } else {
                 error!("Error verifying session {}: {} {}", name, stdout, stderr);
-                return Err("Error reading session.")
+                return Err("Error reading session.");
             }
             r = Ok(stdout);
         } else {
             let _ = fs::remove_file(tmp_name);
             do_create = true; // create session, if we cannot read session - might not yet exist
-            info!("Reading of session {} failed! Trying to create session. {} {}", name, stdout, stderr);
+            info!(
+                "Reading of session {} failed! Trying to create session. {} {}",
+                name, stdout, stderr
+            );
             j["predecessor_key"] = "predecessor".into();
             j["predecessor"] = "~".into();
         };
@@ -106,27 +128,40 @@ pub fn create_session<'a, T : Serialize + for<'de> Deserialize<'de>>(name : &str
             reg.set_strict_mode(true);
             let filename = format!("{tmp_session_dir}/{}", random_name(20));
             {
-                let f = OpenOptions::new().write(true).truncate(true).create(true).open(&filename).expect("Unable to open file '{filename}' (Error 23526-16225-1902)");
+                let f = OpenOptions::new()
+                    .write(true)
+                    .truncate(true)
+                    .create(true)
+                    .open(&filename)
+                    .expect("Unable to open file '{filename}' (Error 23526-16225-1902)");
                 info!("session template={template}");
                 // create session from session template and check if correct
-                let _rendered = reg.render_template_to_write(template, &j, f).expect("error rendering template (Error 5164-30338-3399)");
+                let _rendered = reg
+                    .render_template_to_write(template, &j, f)
+                    .expect("error rendering template (Error 5164-30338-3399)");
             }
             let (code, stdout, stderr) = scone!("scone session check {}", &filename);
             if code != 0 {
-                error!("Session {}: description in '{}' contains errors: {}", &filename, name, stderr);
+                error!(
+                    "Session {}: description in '{}' contains errors: {}",
+                    &filename, name, stderr
+                );
                 // let _ = fs::remove_file(&filename);
                 return Err("Session template seems to be incorrect - have a look at file.");
             }
             info!("Session template for {}: is correct: {}", name, stdout);
 
             // try to create / update the session
-            let (code,stdout, stderr) = scone!("scone session create {}", &filename);
+            let (code, stdout, stderr) = scone!("scone session create {}", &filename);
             // let _ = fs::remove_file(&filename);
             if code == 0 {
                 info!("Created session {}: {}", name, stdout);
                 r = Ok(stdout);
             } else {
-                info!("Creation of session {} failed: {} - see file {}", name, stderr, &filename);
+                info!(
+                    "Creation of session {} failed: {} - see file {}",
+                    name, stderr, &filename
+                );
                 r = Err("failed to create session.")
             }
         }
@@ -136,28 +171,46 @@ pub fn create_session<'a, T : Serialize + for<'de> Deserialize<'de>>(name : &str
     }
 }
 
-pub fn to_json_value<T : Serialize> (o : T) -> serde_json::Value {
-   let r : Value = serde_json::from_str(&serde_json::to_string_pretty(&o).expect("Error serializing Object (Error 22405-15525-20124)")).expect("Error transforming to json object (Error 24639-23448-20309)");
-   r
+pub fn to_json_value<T: Serialize>(o: T) -> serde_json::Value {
+    let r: Value = serde_json::from_str(
+        &serde_json::to_string_pretty(&o)
+            .expect("Error serializing Object (Error 22405-15525-20124)"),
+    )
+    .expect("Error transforming to json object (Error 24639-23448-20309)");
+    r
 }
 
 //fn fromJsonValue<T : Serialize> (o : serde_json::Value) -> T {
 //    let state : T  = serde_json::from_value(&o).expect("Cannot deserialize object");
 //    state
 //}
- 
-pub fn check_mrenclave<'a, T : Serialize + for<'de> Deserialize<'de>> (state: &mut T, mrenclave: &str, image: &str, binary: &str, force: bool) -> Result<(), &'static str> {
-    let mut j : Value = to_json_value(&state);
+
+pub fn check_mrenclave<'a, T: Serialize + for<'de> Deserialize<'de>>(
+    state: &mut T,
+    mrenclave: &str,
+    image: &str,
+    binary: &str,
+    force: bool,
+) -> Result<(), &'static str> {
+    let mut j: Value = to_json_value(&state);
 
     if j[mrenclave] == "" || force {
-        let (code,stdout,stderr)=sh!(r#"docker run --entrypoint="" --rm -e SCONE_HASH=1 {} {} | tr -d '[:space:]'"#, j[image], j[binary]);
+        let (code, stdout, stderr) = sh!(
+            r#"docker run --entrypoint="" --rm -e SCONE_HASH=1 {} {} | tr -d '[:space:]'"#,
+            j[image],
+            j[binary]
+        );
         if code == 0 {
             info!("MrEnclave = {}, stderr={}", stdout, stderr);
             j[mrenclave] = stdout.into();
-            *state = serde_json::from_value(j).expect("deserialization failed (Error 25507-7831-3147)");
+            *state =
+                serde_json::from_value(j).expect("deserialization failed (Error 25507-7831-3147)");
             Ok(())
         } else {
-            error!("Failed to determine MRENCLAVE: {} (Error 13231-21732-26347)", stderr);
+            error!(
+                "Failed to determine MRENCLAVE: {} (Error 13231-21732-26347)",
+                stderr
+            );
             Err("Failed to determine MrEnclave (Error 16676-22493-8368)")
         }
     } else {
@@ -165,29 +218,39 @@ pub fn check_mrenclave<'a, T : Serialize + for<'de> Deserialize<'de>> (state: &m
     }
 }
 
-
 pub trait Init {
     fn new() -> Self;
 }
 
-pub fn write_state<T: Serialize>(state : &T, filename : &str) {
-    let state = serde_json::to_string_pretty(&state).expect("Error serializing internal state (Error 30804-13523-32231)");
+pub fn write_state<T: Serialize>(state: &T, filename: &str) {
+    let state = serde_json::to_string_pretty(&state)
+        .expect("Error serializing internal state (Error 30804-13523-32231)");
     info!("writing state {}", state);
-    fs::write(filename, state).unwrap_or_else(|_| panic!("Unable to write file '{}' (Error 8757-10881-14894)", filename));
+    fs::write(filename, state).unwrap_or_else(|_| {
+        panic!(
+            "Unable to write file '{}' (Error 8757-10881-14894)",
+            filename
+        )
+    });
 }
 
-pub fn read_state<T: Init + for<'de> Deserialize<'de>>(filename : &str) -> T {
+pub fn read_state<T: Init + for<'de> Deserialize<'de>>(filename: &str) -> T {
     if let Ok(state) = fs::read_to_string(filename) {
         info!("Read state {} from {}", state, filename);
-        let state : T  = serde_json::from_str(&state).unwrap_or_else(|_| panic!("Cannot deserialize '{}' (Error 18692-11485-8949)", filename));
+        let state: T = serde_json::from_str(&state).unwrap_or_else(|_| {
+            panic!("Cannot deserialize '{}' (Error 18692-11485-8949)", filename)
+        });
         state
     } else {
-        info!("Failed to read state from file {}: creating this file now. (Warning 4384-20698-6487)", filename);
+        info!(
+            "Failed to read state from file {}: creating this file now. (Warning 4384-20698-6487)",
+            filename
+        );
         T::new()
     }
 }
 
-pub fn random_name(len : usize) -> String {
+pub fn random_name(len: usize) -> String {
     let rand_string: String = thread_rng()
         .sample_iter(&Alphanumeric)
         .take(len)
@@ -195,7 +258,6 @@ pub fn random_name(len : usize) -> String {
         .collect();
     rand_string
 }
-
 
 pub fn get_otp(otp: Option<String>) -> String {
     if let Some(otp) = otp {
@@ -210,9 +272,13 @@ pub fn get_otp(otp: Option<String>) -> String {
         print!("{}", prompt);
 
         // get OTP from user
-        io::stdout().flush().expect("Error flushing stdout (Error 6759-25742-26859)");
+        io::stdout()
+            .flush()
+            .expect("Error flushing stdout (Error 6759-25742-26859)");
         let mut otp = String::new();
-        io::stdin().read_line(&mut otp).expect("Error getting OTP (Error 30189-22111-13542)");
+        io::stdin()
+            .read_line(&mut otp)
+            .expect("Error getting OTP (Error 30189-22111-13542)");
         otp.retain(|c| !c.is_whitespace());
         otp
     }
